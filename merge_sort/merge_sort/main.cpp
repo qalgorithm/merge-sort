8dbc595f47d934ed85264dfec27927ac95f73f0c
Quoc Le <quoc.le@shizzle.co>
2016-10-07 11:15:30 +0700

    first init

---
+++ merge_sort/merge_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  merge_sort
+//
+//  Created by Quoc Le on 10/7/16.
+//  Copyright © 2016 Shizzle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
